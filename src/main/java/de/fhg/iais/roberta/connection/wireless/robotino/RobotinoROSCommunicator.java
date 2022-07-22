4cf6cb3698535e03a3c3594cd3214db297e3d4a5
heini208 <mstemmeler@gmx.de>
2022-07-22 16:44:53 +0200

    Last python program now gets killed before uploading new one

--- src/main/java/de/fhg/iais/roberta/connection/wireless/robotino/RobotinoROSCommunicator.java
+++ src/main/java/de/fhg/iais/roberta/connection/wireless/robotino/RobotinoROSCommunicator.java
@@ -1,12 +1,10 @@
 package de.fhg.iais.roberta.connection.wireless.robotino;
 
 import de.fhg.iais.roberta.connection.wireless.IWirelessCommunicator;
-import de.fhg.iais.roberta.util.PropertyHelper;
 import de.fhg.iais.roberta.util.SshConnection;
 import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.userauth.UserAuthException;
-import org.apache.commons.lang3.SystemUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,25 +26,17 @@ public class RobotinoROSCommunicator implements IWirelessCommunicator {
 
     private String password = "robotino";
 
-    private final String workingDirectory;
-    private String firmwareVersion = "";
+    private String firmwareVersion = "1.0";
 
     public RobotinoROSCommunicator(Robotino robotino) {
         this.name = robotino.getName();
         this.address = robotino.getAddress();
-
-        if ( SystemUtils.IS_OS_WINDOWS ) {
-            this.workingDirectory = System.getenv("APPDATA") + '/' + PropertyHelper.getInstance().getProperty("artifactId") + '/';
-        } else {
-            this.workingDirectory = System.getProperty("user.home") + '/' + PropertyHelper.getInstance().getProperty("artifactId") + '/';
-        }
     }
 
     /**
      * Uploads a binary file to the Robotino robot.
      *
      * @param binaryFile the content of the file
-     * @param fileName   the desired file name
      * @throws UserAuthException if the user is not correctly authorized
      * @throws IOException if something with the ssh connection or file transfer went wrong
      */
@@ -55,13 +45,16 @@ public class RobotinoROSCommunicator implements IWirelessCommunicator {
             password = "robotino";
         }
         try (SshConnection ssh = new SshConnection(this.address, USERNAME, this.password)) {
+            //kill last running python Program
+            ssh.command("pkill -1 -f " + fileName);
             ssh.copyLocalToRemote(binaryFile, "/home/robotino/robertaPrograms", fileName);
+
             LOG.info("starting roslaunch...");
             //execute launch script and immediately move on
 
             ssh.command("workspacePath=$(/home/robotino/robertaPrograms/getWorkspace.sh);" +
                     " source /opt/ros/noetic/setup.bash; source  $workspacePath" +
-                   " ;bash /home/robotino/robertaPrograms/launchRobertaScript.sh " + fileName + "&> /dev/null & disown $!");
+                    " ;bash /home/robotino/robertaPrograms/launchRobertaScript.sh " + fileName + "&> /dev/null & disown $!");
         } catch ( FileNotFoundException | TransportException | ConnectionException e ) {
             throw new IOException(e);
         }
@@ -91,7 +84,7 @@ public class RobotinoROSCommunicator implements IWirelessCommunicator {
         return deviceInfo;
     }
 
-    public String checkFirmwareVersion() throws UserAuthException, IOException {
+    public String checkFirmwareVersion() throws IOException {
         try (SshConnection ssh = new SshConnection(this.address, USERNAME, this.password)) {
             String msg = "CHANGETHIS";//ssh.command("naoqi-bin --version");
             String version = "CHANGETHIS";//msg.split("\n")[0].split(":")[1].trim();
